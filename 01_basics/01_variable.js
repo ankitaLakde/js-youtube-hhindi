e879d652433a751cfb182609fbc579cff95fc82d
ankitaLakde <80130604+ankitaLakde@users.noreply.github.com>
2025-07-22 17:48:49 +0000

    variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,19 @@
+const accountId=1324
+let accountEmail="ankita@gmail.com"
+var accountPassword="jhjhb"
+accountLocation="jalna"
+
+let accountState; //here we didn't pass any value then it will shown as undefined
+
+/* 
+prefer not to use var
+because of issue in block acope & functional scope
+*/
+// accountId=2//not allowed
+console.log(accountId);
+accountEmail="assd@gmail.com"
+accountPassword="lol"
+accountLocation="Chhatrapati Sambhaji Nagar" 
+
+console.table([accountEmail, accountId, accountPassword, accountLocation,accountState]);
+
