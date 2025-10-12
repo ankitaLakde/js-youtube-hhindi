01d6e82f52fca664924aeb51156b65fb18993d35
ankitaLakde <80130604+ankitaLakde@users.noreply.github.com>
2025-10-12 17:54:52 +0000

    test new

---
+++ 01_basics/03_conversiontype.js
@@ -0,0 +1,31 @@
+let score="66";
+// let score="3221a";//showing NAN
+// let score=null;// value is 0
+// let score=undefined;//NAN value showing
+// let score=true; value is in boolean
+
+console.log(typeof score);
+console.log(typeof(score));
+
+
+let valueInNumber=Number(score)
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+
+
+// "33"=> 33
+// "33acd"=> Not A Number
+// true=>1
+// false=>0
+
+// let isLoggedIn = 1
+let isLoggedIn = "hvvhgvh"//true
+// let isLoggedIn = ""//empty
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+let someNumber = 333
+let stringNumber =  String(someNumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber));
+
