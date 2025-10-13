ff276f04760a10ff2a12e29026a225031f91b941
ankitaLakde <80130604+ankitaLakde@users.noreply.github.com>
2025-10-13 07:28:49 +0000

    conversion

---
+++ 01_basics/04_conversionopration.js
@@ -0,0 +1,44 @@
+// console.log(2+3);
+// console.log(3-2);
+// console.log(3*2);
+// console.log(3/2);
+// console.log(3%2);
+
+str1="Hello"
+str2=" ..Ankita"
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("2"+2+3);
+// console.log(2+3+"2");
+// console.log(+true);
+// console.log(+"");
+
+
+
+let num1,num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gamecounter=100
+gamecounter++
+// console.log(gamecounter);
+ 
+
+// console.log("2">1);
+// console.log("02">1);
+console.log(null>0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined>0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+// ===
+console.log("2"===2);
+
+
+
+
+
---
+++ 01_basics/05_datatype_summery.js
@@ -0,0 +1,33 @@
+// #Primitive
+
+// 7 Types:String, number, Boolean , Null, undefined, Symbol(unquie value), BigInt
+
+const score= 100
+const scoreValue=11.1
+const isLoggedIn=false
+const outSideTemp=null
+
+let userEmail
+// const id= Symbol('1222')
+// const anotherId = symbol('123')
+
+// console.log(id == anotherId);
+
+const bigNumber = 33554564569999n
+
+// Non Primitive(Reference Type)
+// Array, objects, functions
+const hero=["l1", "l2", "l3"];
+const myObj={
+    name:"jkbhjb",
+    age:22,
+}
+const myFunction=function(){
+    console.log("Heloo World");
+    
+}
+
+console.log(typeof(myFunction))
+
+
+
